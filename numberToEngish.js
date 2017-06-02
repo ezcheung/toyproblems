b4af624fb41b3cc07dc8fa3a9bb22ec1cb136b03
Elliot Cheung <elliotccheung@gmail.com>
2017-06-02 10:44:09 +0100

    Started numberToEnglish

---
+++ numberToEngish.js
@@ -0,0 +1,8 @@
+/**
+Write a function numberToEnglish, it should take a number and return the number as a string using English words.
+helper objects: numbersToWords, numbersToPlace
+*/
+
+function numberToEnglish(number) {
+  
+}
