f9841aa51fd0b3dbed67ea9c986ae56704dc62df
Elliot Cheung <elliotccheung@gmail.com>
2017-06-02 11:14:57 +0100

    Wrote helper function to parse tens place

--- numberToEngish.js
+++ numberToEngish.js
@@ -4,5 +4,19 @@ helper objects: numbersToWords, numbersToPlace
 */
 
 function numberToEnglish(number) {
-  
+  if(number === 0) return 'zero';
+  ntw = (num) => numbersToWords[num.toString()];
+  numbersToPlace = ['thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion'];
+
+  function tens(number) {
+    if(number === 0) return '';
+    if(number < 21) return ntw(number) + " " + numbersToPlace[placeIndex];
+    var ten = ntw(Math.floor(number / 10) * 10);
+    var one = number % 10;
+    one = one ? '-' + ntw(one) : '';
+    return ten + one;
+  }
+  function parseBlock(number, placeIndex) {
+    var hundreds = ntw(Math.floor(number / 100)) + ' hundred ';
+  }
 }
