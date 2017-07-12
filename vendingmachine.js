7fdd44ed9b6ca24fc49136b6924b77366d13b08e
Elliot Cheung <elliotccheung@gmail.com>
2017-07-12 12:08:14 -0500

    Exact price and not enough price works, changemaking still not working

--- vendingmachine.js
+++ vendingmachine.js
@@ -17,3 +17,53 @@ It will return any money that is not valid for the machine.
 If it can not return the exact change out, it returns the least amount that is closest to the change out. The machine never loses money!
 */
 
+function VendingMachine(coins) {
+  this.coins = coins;
+}
+
+VendingMachine.prototype.vending = function(price, credit) {
+  let returnCoins = {};
+
+  let paid = 0;
+  for(let c in credit) {
+    if(this.coins[c] === undefined) {
+      // invalid coin
+      returnCoins[c] = credit[c];
+      continue;
+    }
+    paid += credit[c] * c;
+  }
+
+  if(paid < price) return credit;
+  if(paid === price) return returnCoins;
+  
+  let change = paid - price;
+  let validCoins = Object.keys(this.coins).sort((a, b) => b-a);
+  let coinStack = [];
+  function changeMake(left=change, closestStack=[], closest = change) {
+    if(left === 0) return true;
+    for(let c = 0; c < validCoins.length; c++) {
+      if(left >= validCoins[c]) {
+        coinStack.push(validCoins[c]);
+        this.coins[validCoins[c]] -= 1;
+        if(changeMake(left - validCoins[c], closestStack, closest) === true){
+          return true;
+        }
+        let value = coinStack.reduce((a, e) => a+e, 0);
+        if(value < closest) {
+          closest = value;
+          closestStack = coinStack.slice();
+        }
+        let popped = coinStack.pop();
+        this.coins[popped] += 1;
+      }
+    }
+    return false;
+  }
+  console.log("Coin Stack: ", coinStack);
+  for(let i = 0; i < coinStack.length; i++) {
+    if(returnCoins[coinStack[i]] === undefined) returnCoins[coinStack[i]] = 0;
+    returnCoins[coinStack[i]] += 1;
+  }
+  return returnCoins;
+}
