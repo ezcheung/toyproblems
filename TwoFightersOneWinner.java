ff62fee12e66da025a4eedc95aae1bf5aabda809
Elliot Cheung <elliotccheung@gmail.com>
2017-07-19 15:03:19 -0500

    Started TwoFightersOneWinner.java

---
+++ TwoFightersOneWinner.java
@@ -0,0 +1,13 @@
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+public class Kata {
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    // Your code goes here. Have fun!
+  }
+}
