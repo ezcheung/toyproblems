4dabba3724721b1e3738f2ad34e511927e3782dd
Elliot Cheung <elliotccheung@gmail.com>
2017-07-19 15:12:11 -0500

    Solved TwoFightersOneWinner.java

--- TwoFightersOneWinner.java
+++ TwoFightersOneWinner.java
@@ -6,8 +6,42 @@
 
 // Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
 
+// Given in instructions
+public class Fighter {
+  public String name;
+  public int health, damagePerAttack;
+  public Fighter(String name, int health, int damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+  }
+}
+
 public class Kata {
   public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
-    // Your code goes here. Have fun!
+    if(firstAttacker == fighter2.name) {
+      Fighter tmp = fighter1;
+      fighter1 = fighter2;
+      fighter2 = tmp;
+    }
+
+    String winner;
+    while(true) {
+      if(fighter1.health <= 0) {
+        winner = fighter2.name;
+        break;
+      }
+      else {
+        fighter2.health -= fighter1.damagePerAttack;
+      }
+      if(fighter2.health <= 0) {
+        winner = fighter1.name;
+        break;
+      }
+      else {
+        fighter1.health -= fighter2.damagePerAttack;
+      }
+    }
+    return winner;
   }
 }
