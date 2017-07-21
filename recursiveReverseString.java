298ed7da3cf6411843975956fb8e97004a2815cf
Elliot Cheung <elliotccheung@gmail.com>
2017-07-20 19:37:35 -0500

    Made small correction in recursiveReverseString.java

--- recursiveReverseString.java
+++ recursiveReverseString.java
@@ -11,7 +11,7 @@
 public class Reverse {
 
     public String reverse(String str) {
-       if(str.length() == 1) return str;
+       if(str.length() <= 1) return str;
        else return str.charAt(str.length() - 1) + reverse(str.substring(0, str.length() - 1));
     }
 }
