e963cbe6ca91e988a215ec07b6275a1d6d791b69
Elliot Cheung <elliotccheung@gmail.com>
2017-07-20 19:38:33 -0500

    Started mostDigits.java

---
+++ mostDigits.java
@@ -0,0 +1,11 @@
+/**
+* Find the number with the most digits.
+* 
+* If two numbers in the argument array have the same number of digits, return the first one in the array.
+*/
+
+public class MostDigits {
+  public static int findLongest(int[] numbers) {
+  
+  }
+}
