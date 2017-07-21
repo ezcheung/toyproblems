49bcb531ed0571807bd2accdf8e63b152a88c630
Elliot Cheung <elliotccheung@gmail.com>
2017-07-20 19:47:36 -0500

    Solved mostDigits.java

--- mostDigits.java
+++ mostDigits.java
@@ -6,6 +6,15 @@
 
 public class MostDigits {
   public static int findLongest(int[] numbers) {
-  
+    if(numbers.length <= 0) return 0;
+    int longIndex = 0;
+    for(int i = 1; i < numbers.length; i++) {
+      int number = numbers[i];
+      int longNum = numbers[longIndex];
+      if(number < 0) number = number * -1;
+      if(longNum < 0) longNum = longNum * -1;
+      if((number + "").length() > (longNum + "").length()) longIndex = i;
+    }
+    return numbers[longIndex];
   }
 }
