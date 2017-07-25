74649aa7363b76634913691d44d09bf6a3f1b7b6
Elliot Cheung <elliotccheung@gmail.com>
2017-07-25 10:53:12 -0500

    Started oddSort.java

---
+++ oddSort.java
@@ -0,0 +1,11 @@
+// You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+// Zero isn't an odd number and you don't need to move it. 
+// If you have an empty array, you need to return it.
+
+public class Kata {
+  public static int[] sortArray(int[] array) {
+    return array;
+  }
+}
