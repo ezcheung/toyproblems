c72882e0c453ef2adc6bef7f916b751da9be61bc
Elliot Cheung <elliotccheung@gmail.com>
2017-08-31 14:27:20 -0500

    Finished exesandohs.js

--- exesandohs.js
+++ exesandohs.js
@@ -1,3 +1,7 @@
 /**
 Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contains any char.
 */
+
+function XO(str) {
+  return !str.length ? true : str.match(/[oO]/g) !== null && str.match(/[xX]/g) !== null && str.match(/[oO]/g).length === str.match(/[xX]/g).length;
+}
